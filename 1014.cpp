bc994c9bcffbce14e5a46a62e12f9fda66ac5c5f
BiBaIsAFish <s098777336@gmail.com>
2024-12-27 19:11:35 +0800

    2024/12/27 ver2

--- 1014.cpp
+++ 1014.cpp
@@ -2,19 +2,32 @@
 
 using namespace std;
 
-// 0ms, 100.00%; 47.29MB, 28.33%;
-
 class Solution {
 public:
+    // 0ms, 100.00%; 47.29MB, 28.33%;
+    // int maxScoreSightseeingPair(vector<int>& values) {
+    //     int n = values.size();
+    //     vector<int> dp(n);
+    //     dp[0] = values[0];
+    //     int res = 0;
+    //     for (int i = 1; i < n; ++i) {
+    //         int curr = values[i];
+    //         res = max(curr + dp[i-1] - 1, res);
+    //         dp[i] = max(curr, dp[i-1] - 1);
+    //     }
+
+    //     return res;
+    // }
+
+    // one integer is sufficient;
+    // 0ms, 100.00%; 45.50MB, 65.45%;
     int maxScoreSightseeingPair(vector<int>& values) {
-        int n = values.size();
-        vector<int> dp(n);
-        dp[0] = values[0];
+        int dp = values[0];
         int res = 0;
-        for (int i = 1; i < n; ++i) {
+        for (int i = 1; i < values.size(); ++i) {
             int curr = values[i];
-            res = max(curr + dp[i-1] - 1, res);
-            dp[i] = max(curr, dp[i-1] - 1);
+            res = max(curr + dp - 1, res);
+            dp = max(dp-1, curr);
         }
 
         return res;
